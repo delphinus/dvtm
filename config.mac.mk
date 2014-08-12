89018bdb85078e4acfdc5ee975590b90abd71aff
delphinus <delphinus@remora.cx>
2014-08-13 01:38:16 +0900

    developing5

--- config.mac.mk
+++ config.mac.mk
@@ -9,8 +9,9 @@ MANPREFIX = ${PREFIX}/share/man
 INCS = -I. -I/usr/include -I/usr/local/include
 LIBS = -lc -lutil -lcurses
 
-CFLAGS += -std=c99 -Os ${INCS} -DVERSION=\"${VERSION}\" -DNDEBUG
-LDFLAGS += -L/usr/lib -L/usr/local/lib ${LIBS}
+CFLAGS += -m32 -std=c99 -Os ${INCS} -DVERSION=\"${VERSION}\" -DNDEBUG
+LDFLAGS += -L/usr/lib -L/usr/local/lib -L/usr/local/opt/ncurses/lib ${LIBS}
+CPPFLAGS += -I/usr/local/opt/ncurses/include
 
 DEBUG_CFLAGS = ${CFLAGS} -UNDEBUG -O0 -g -ggdb -Wall -Wextra -Wno-missing-field-initializers -Wno-unused-parameter
 
