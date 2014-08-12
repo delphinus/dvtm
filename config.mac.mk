fb1621d4e3f358eca1d0abde7691bc6eca30c48a
delphinus <delphinus@remora.cx>
2014-08-13 01:39:53 +0900

    developing5

--- config.mac.mk
+++ config.mac.mk
@@ -10,7 +10,7 @@ INCS = -I. -I/usr/include -I/usr/local/include
 LIBS = -lc -lutil -lcurses
 
 CFLAGS += -m32 -std=c99 -Os ${INCS} -DVERSION=\"${VERSION}\" -DNDEBUG
-LDFLAGS += -L/usr/lib -L/usr/local/lib -L/usr/local/opt/ncurses/lib ${LIBS}
+LDFLAGS += -L/usr/local/opt/ncurses/lib -L/usr/lib -L/usr/local/lib ${LIBS}
 CPPFLAGS += -I/usr/local/opt/ncurses/include
 
 DEBUG_CFLAGS = ${CFLAGS} -UNDEBUG -O0 -g -ggdb -Wall -Wextra -Wno-missing-field-initializers -Wno-unused-parameter
